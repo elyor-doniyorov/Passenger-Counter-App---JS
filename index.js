cd56c5cceffaacdbcce5784c35312812195090ab
elyor doniyorov <doniyorovelyor@gmail.com>
2021-08-02 19:41:05 +0500

    Increment on clicks

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-let lapsCompleted = 0
+// document.getElementById("count").innerText = 5
 
-// Create a function that increments the lapsCompleted variable with one
-// Run it three times
+// let count = 5
+// count = count + 1
+// console.log(count)
 
-function incNum() {
-    lapsCompleted = lapsCompleted + 1   
+// initialize the count as 0
+let count = 0
+
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked(log it out)
+function increment() {
+    count = count + 1
+    console.log(count)
+    
 }
 
-incNum()
-incNum()
-incNum()
+// increment()
+// increment()
+// console.log(count)
 
-console.log(lapsCompleted) 
+
+// change the count-el in the HTML to reflect the new count
+
+
