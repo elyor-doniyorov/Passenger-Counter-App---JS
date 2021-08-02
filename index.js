114e69c41125e530db1f3f1dcc1fa67b54447b1a
elyor doniyorov <doniyorovelyor@gmail.com>
2021-08-03 00:18:49 +0500

    Add greeting text

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// document.getElementById("count").innerText = 5
-
 // change the count-el in the HTML to reflect the new count
 let countEl = document.getElementById("count-el")
 
@@ -16,9 +14,18 @@ function increment() {
 function save() {
     console.log(count)
 }
-
 save()
 
+// Grab the welcome-el paragraph and store it in a variable called welcomeEl
+let welcomeEl = document.getElementById("welcome-el")
+
+// Create two variables (name & greeting) that contains your name
+// and the greeting we want to render on the page
+let myName = "Elyor Doniyorov"
+let greeting = "Welcome back, "
+
+// Render the welcome message using welcomeEl.innerText
+welcomeEl.innerText = greeting + myName
 
 
 
