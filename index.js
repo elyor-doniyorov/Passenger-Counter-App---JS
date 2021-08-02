865fe1b8eb84fae219d5b22d9449668fa1d85503
elyor doniyorov <doniyorovelyor@gmail.com>
2021-08-02 19:24:05 +0500

    Write a function that implements

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-let lap1 = 34
-let lap2 = 33
-let lap3 = 36
+let lapsCompleted = 0
 
-// Create a function that logs out the sum of all the lap times
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
 
-function lapTime() {
-    let totalTime = lap1 + lap2 + lap3
-    console.log(totalTime)
+function incNum() {
+    lapsCompleted = lapsCompleted + 1   
 }
 
-// lapTime()
-console.log(totalTime)
+incNum()
+incNum()
+incNum()
+
+console.log(lapsCompleted) 
