e28acd0755dd4fd99e5884584178ef760dfbdfda
elyor doniyorov <doniyorovelyor@gmail.com>
2021-08-02 19:51:23 +0500

    Display the count

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 // document.getElementById("count").innerText = 5
 
-// let count = 5
-// count = count + 1
-// console.log(count)
+// change the count-el in the HTML to reflect the new count
+let countEl = document.getElementById("count-el")
 
-// initialize the count as 0
+console.log(countEl)
+
 let count = 0
 
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked(log it out)
 function increment() {
     count = count + 1
-    console.log(count)
+    countEl.innerText = count
     
 }
 
-// increment()
-// increment()
-// console.log(count)
 
 
-// change the count-el in the HTML to reflect the new count
 
 
