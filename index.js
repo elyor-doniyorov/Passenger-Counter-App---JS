6fc71465cc6713e139c8bae641c06a8eeeba70e1
elyor doniyorov <doniyorovelyor@gmail.com>
2021-08-02 19:14:34 +0500

    Writing a function that logs the sum

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function sayNum() {
-    console.log(42)
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+// Create a function that logs out the sum of all the lap times
+
+function lapTime() {
+    let totalTime = lap1 + lap2 + lap3
+    console.log(totalTime)
 }
 
-sayNum()
+// lapTime()
+console.log(totalTime)
