6a04ca81cd9e862f29bfad19dbe205b116769cf4
elyor doniyorov <doniyorovelyor@gmail.com>
2021-08-02 16:54:27 +0500

    Basic mathematical operations

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-document.getElementById("counter-el").innerText = 5;
+// document.getElementById("count-el").innerText = 5
+
+let firstBatch = 5
+let secondBatch = 7
+
+let count = firstBatch + secondBatch
+
+console.log(count)
